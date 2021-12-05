474d9c2cae47e21b3ca2fd6219912f84b745acc0
Matthew Read <MatthewRyanRead@users.noreply.github.com>
2021-12-05 01:12:09 -0500

    improve harness

--- harness.sh
+++ harness.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-java "Day$1.java"
+set -e
 
+if [[ "$(echo $1 | grep -oE '^(10)|(1?[1-9])|(2[0-5])$')" != "$1" ]]; then
+    echo 'Argument must be a valid advent day (between 1 and 25)'
+    exit 1
+fi
+
+JAVA_MAJ_VER=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | grep -oE '^[0-9]+')
+if (( $JAVA_MAJ_VER < 11 )); then
+    echo 'Java 11+ is required'
+    exit 1
+fi
+
+java --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED "Day$1.java"
