8d7dad75c4556c7ce512df3891079a6005058ff5
Matthew Read <MatthewRyanRead@users.noreply.github.com>
2021-12-19 16:03:11 -0500

    day 19 part 1 -- nasty but it works

--- Day19.java
+++ Day19.java
@@ -3,6 +3,7 @@ package tech.readonly.aoc2021;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -14,12 +15,18 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static tech.readonly.aoc2021.Day19.Beacon.Swap;
+import static tech.readonly.aoc2021.Day19.Beacon.Swap.SWAPS;
+import static tech.readonly.aoc2021.Day19.Beacon.Swap.XYZ;
+import static tech.readonly.aoc2021.Day19.Beacon.Transform;
+import static tech.readonly.aoc2021.Day19.Beacon.Transform.DEFAULT;
+import static tech.readonly.aoc2021.Day19.Beacon.Transform.TRANSFORMS;
+
 public class Day19 {
     public static void main(final String[] args) throws Exception {
         final Set<Scanner> scanners = new HashSet<>();
 
-        try (final java.util.Scanner input =
-                new java.util.Scanner(new File("inputs/samples/day19.txt"))) {
+        try (final java.util.Scanner input = new java.util.Scanner(new File("inputs/day19.txt"))) {
             Scanner currScanner = null;
 
             while (input.hasNextLine()) {
@@ -57,8 +64,8 @@ public class Day19 {
             final Scanner referenceScanner = transformedScanners.get(referenceScannerIndex);
             final List<Scanner> matchesFound = new ArrayList<>();
             final List<Scanner> scannerList = new ArrayList<>(scanners);
-            boolean breakout = false;
 
+            findMatch:
             for (final Scanner scannerToCheck : scannerList) {
                 for (int i = 0; i < referenceScanner.beacons.size(); i++) {
                     final Set<BeaconDistance> refDistances =
@@ -71,18 +78,74 @@ public class Day19 {
                         checkDistances.retainAll(refDistances);
 
                         if (checkDistances.size() >= 11) {
-                            // TODO: need to re-orient
+                            final BeaconDistance checkDistance =
+                                    checkDistances.stream().findFirst().get();
+                            final BeaconDistance refDistance =
+                                    refDistances.stream()
+                                            .filter(
+                                                    rd ->
+                                                            rd.distSet.containsAll(
+                                                                    checkDistance.distSet))
+                                            .findFirst()
+                                            .get();
+                            Transform transform = DEFAULT;
+                            Swap swap = XYZ;
+
+                            transformFinder:
+                            for (int k = 0; k < TRANSFORMS.size(); k++) {
+                                transform = TRANSFORMS.get(k);
+
+                                for (int m = 0; m < SWAPS.size(); m++) {
+                                    swap = SWAPS.get(m);
+                                    int[] xyz =
+                                            transform.transform(
+                                                    checkDistance.distList.get(0),
+                                                    checkDistance.distList.get(1),
+                                                    checkDistance.distList.get(2));
+                                    xyz = swap.transform(xyz[0], xyz[1], xyz[2]);
+                                    final List<Integer> xyzList =
+                                            Arrays.stream(xyz).boxed().collect(Collectors.toList());
+
+                                    if (xyzList.equals(refDistance.distList)) {
+                                        break transformFinder;
+                                    }
+                                }
+                            }
+
+                            final Beacon unscaledTransform =
+                                    new Beacon(
+                                            checkDistance.beacon1,
+                                            transform,
+                                            swap,
+                                            new int[] {0, 0, 0});
+                            final int[] offset =
+                                    new int[] {
+                                        unscaledTransform.x - refDistance.beacon1.x,
+                                        unscaledTransform.y - refDistance.beacon1.y,
+                                        unscaledTransform.z - refDistance.beacon1.z,
+                                    };
+
+                            final List<Beacon> beaconsToTransform =
+                                    new ArrayList<>(scannerToCheck.beacons);
+                            scannerToCheck.beacons.clear();
+
+                            for (final Beacon beaconToTransform : beaconsToTransform) {
+                                scannerToCheck.beacons.add(
+                                        new Beacon(beaconToTransform, transform, swap, offset));
+                            }
+                            scannerToCheck.distances.clear();
+                            scannerToCheck.calcDistances();
+                            scannerToCheck.position[0] = -offset[0];
+                            scannerToCheck.position[1] = -offset[1];
+                            scannerToCheck.position[2] = -offset[2];
+
                             matchesFound.add(scannerToCheck);
                             scanners.remove(scannerToCheck);
-                            breakout = true;
-                            break;
+
+                            break findMatch;
                         }
                     }
-
-                    if (breakout) break;
                 }
-
-                if (breakout) break;
             }
 
             if (matchesFound.isEmpty()
@@ -93,14 +156,35 @@ public class Day19 {
             transformedScanners.addAll(matchesFound);
         } while (!scanners.isEmpty());
 
-        System.out.println(
-                scanners.stream().map(Scanner::toString).collect(Collectors.joining("\n\n")));
+        /*System.out.println(
+        transformedScanners.stream()
+                .sorted(Comparator.comparingInt(s -> s.id))
+                .map(Scanner::toString)
+                .collect(Collectors.joining("\n\n")));*/
+
+        final Set<Beacon> allBeacons =
+                transformedScanners.stream()
+                        .flatMap(s -> s.beacons.stream())
+                        .collect(Collectors.toSet());
+        allBeacons.removeIf(
+                b ->
+                        allBeacons.stream()
+                                .anyMatch(
+                                        b2 ->
+                                                b != b2
+                                                        && b.x == b2.x
+                                                        && b.y == b2.y
+                                                        && b.z == b2.z));
+
+        System.out.println("Part 1: " + allBeacons.size());
     }
 
     private static class Scanner {
         private final int id;
         private final List<Beacon> beacons = new ArrayList<>();
         private final Map<Beacon, Set<BeaconDistance>> distances = new HashMap<>();
+
+        private int[] position = {0, 0, 0};
 
         private Scanner(final int id) {
             this.id = id;
@@ -125,11 +209,17 @@ public class Day19 {
         public String toString() {
             return String.format(
                     "--- scanner %d ---%n%s",
-                    id, beacons.stream().map(Beacon::toString).collect(Collectors.joining("\n")));
+                    id,
+                    beacons.stream()
+                            .sorted(Comparator.comparingInt(b -> b.z))
+                            .sorted(Comparator.comparingInt(b -> b.y))
+                            .sorted(Comparator.comparingInt(b -> b.x))
+                            .map(Beacon::toString)
+                            .collect(Collectors.joining("\n")));
         }
     }
 
-    private static class Beacon {
+    static class Beacon {
         private final int x, y, z;
         private final Transform transform;
         private final Swap swap;
@@ -145,15 +235,17 @@ public class Day19 {
             swap = Swap.XYZ;
         }
 
-        private Beacon(final Beacon beacon, final Transform transform, final Swap swap) {
-            int[] xyz = beacon.swap.reverse(beacon.x, beacon.y, beacon.z);
-            xyz = beacon.transform.reverse(xyz[0], xyz[1], xyz[2]);
-            xyz = transform.transform(xyz[0], xyz[1], xyz[2]);
+        private Beacon(
+                final Beacon beacon,
+                final Transform transform,
+                final Swap swap,
+                final int[] offset) {
+            int[] xyz = transform.transform(beacon.x, beacon.y, beacon.z);
             xyz = swap.transform(xyz[0], xyz[1], xyz[2]);
 
-            this.x = xyz[0];
-            this.y = xyz[1];
-            this.z = xyz[2];
+            this.x = xyz[0] - offset[0];
+            this.y = xyz[1] - offset[1];
+            this.z = xyz[2] - offset[2];
             this.transform = transform;
             this.swap = swap;
         }
@@ -163,7 +255,7 @@ public class Day19 {
             return String.format("%d,%d,%d", x, y, z);
         }
 
-        private enum Transform {
+        enum Transform {
             DEFAULT(new int[] {1, 1, 1}),
             NEGATE_X(new int[] {-1, 1, 1}),
             NEGATE_Y(new int[] {1, -1, 1}),
@@ -173,7 +265,7 @@ public class Day19 {
             NEGATE_YZ(new int[] {1, -1, -1}),
             NEGATE_XYZ(new int[] {-1, -1, -1});
 
-            private static final Map<Integer, Transform> TRANSFORMS =
+            static final Map<Integer, Transform> TRANSFORMS =
                     Arrays.stream(Transform.values())
                             .collect(Collectors.toMap(Enum::ordinal, Function.identity()));
             private final int[] matrix;
@@ -191,7 +283,7 @@ public class Day19 {
             }
         }
 
-        private enum Swap {
+        enum Swap {
             XYZ(new int[] {0, 1, 2}),
             XZY(new int[] {0, 2, 1}),
             YXZ(new int[] {1, 0, 2}),
@@ -199,7 +291,7 @@ public class Day19 {
             ZXY(new int[] {1, 2, 0}),
             ZYX(new int[] {2, 1, 0});
 
-            private static final Map<Integer, Swap> SWAPS =
+            static final Map<Integer, Swap> SWAPS =
                     Arrays.stream(Swap.values())
                             .collect(Collectors.toMap(Enum::ordinal, Function.identity()));
             private final int[] positions;
