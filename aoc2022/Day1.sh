56b4270413e743fcbb605372fd2678cd04abe7eb
Matthew Read <MatthewRyanRead@users.noreply.github.com>
2022-12-01 10:58:22 -0500

    2022 day 1; Bash

---
+++ aoc2022/Day1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+max1=0
+max2=0
+max3=0
+curr=0
+
+while read line; do
+    if [[ "$line" == "" ]]; then
+        if (( $curr > $max1 )); then
+            max3=$max2
+            max2=$max1
+            max1=$curr
+        elif (( $curr > $max2 )); then
+            max3=$max2
+            max2=$curr
+        elif (( $curr > $max3 )); then
+            max3=$curr
+        fi
+
+        curr=0
+    else
+        curr=$(($curr + $line))
+    fi
+done <<< "$(cat $1)"
+
+echo "Max: $max1"
+echo "Top 3 sum: $(($max1 + $max2 + $max3))"
