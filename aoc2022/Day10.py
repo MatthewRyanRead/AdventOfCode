e625357ca5f5d7f820585f2e2e5749df5ae1940d
Matthew Read <MatthewRyanRead@users.noreply.github.com>
2022-12-10 01:33:22 -0500

    2022 day 10; Python

---
+++ aoc2022/Day10.py
@@ -0,0 +1,44 @@
+import requests
+
+input_uri = 'https://raw.githubusercontent.com/MatthewRyanRead/AdventOfCode/main/aoc2022/inputs/Day10.txt'
+response = requests.get(input_uri, stream=True)
+response.raw.decode_content = True
+input = response.raw.read().decode('utf-8').splitlines()
+
+addends_by_step = {}
+x = 1
+score = 0
+message = ''
+idx = 0
+step = 0
+delay = False
+
+while idx < len(input):
+    step += 1
+    x += addends_by_step.pop(step, 0)
+
+    if (step-20)%40 == 0:
+        score += x * step
+    
+    screen_pos = (step - 1) % 40
+    if screen_pos == 0:
+        message += '\n'
+    if screen_pos in [x - 1, x, x + 1]:
+        message += '#'
+    else:
+        message += '.'
+
+    if delay:
+        delay = False
+        continue
+
+    line = input[idx]
+    idx += 1
+    instruction = line[:4]
+
+    if instruction == 'addx':
+        delay = True
+        addends_by_step[step + 2] = int(line[5:])
+
+print('Part 1:', score)
+print('Part 2:', message)
