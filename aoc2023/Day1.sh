f254edcb5d9f8c8b937c7c4baa429cc5408ed681
Matthew Read <MatthewRyanRead@users.noreply.github.com>
2023-12-02 14:34:27 -0500

    better function name

--- aoc2023/Day1.sh
+++ aoc2023/Day1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-parse_number() {
+solve() {
     echo "$1" | sed 's/[^0-9]//g' \
         | while read line; do echo "${line:0:1}${line: -1}"; done \
         | gawk '{s+=$1;} END {print s;}'
@@ -9,5 +9,5 @@ parse_number() {
 input=$(cat inputs/Day1.txt)
 part2=$(echo "$input" | sed 's/one/one1one/g' | sed 's/two/two2two/g' | sed 's/three/three3three/g' | sed 's/four/four4four/g' | sed 's/five/five5five/g' | sed 's/six/six6six/g' | sed 's/seven/seven7seven/g' | sed 's/eight/eight8eight/g' | sed 's/nine/nine9nine/g')
 
-echo Part 1: $(parse_number "$input")
-echo Part 2: $(parse_number "$part2")
+echo Part 1: $(solve "$input")
+echo Part 2: $(solve "$part2")
