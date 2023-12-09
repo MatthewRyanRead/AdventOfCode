9c3b74cf9f67ef8831e00ccf1ffad04f8304766c
Matthew Read <MatthewRyanRead@users.noreply.github.com>
2023-12-09 17:47:50 -0500

    2023 day 9; Kotlin

---
+++ aoc2023/Day9.kt
@@ -0,0 +1,51 @@
+﻿import java.io.File
+import java.util.Scanner
+import java.util.function.Predicate
+import kotlin.math.max
+
+fun main() {
+    val input = mutableListOf<List<Long>>()
+    Scanner(File("inputs/Day9.txt")).use {
+        while (it.hasNextLine()) {
+            input.add(it.nextLine().trim().split(' ').map { s -> s.toLong() })
+        }
+    }
+
+    println("Part 1: ${solve(input, false)}")
+    println("Part 2: ${solve(input, true)}")
+}
+
+fun solve(input: List<List<Long>>, isPart2: Boolean): Long {
+    val answers = mutableListOf<Long>()
+    for (reading in input) {
+        val differences = mutableListOf<List<Long>>()
+        differences.add(reading)
+
+        var last = reading
+        while (!last.all { it == 0L }) {
+            val newDifferences = mutableListOf<Long>()
+            for (i in 0..<(last.size - 1)) {
+                newDifferences.add(last[i + 1] - last[i])
+            }
+
+            differences.add(newDifferences)
+            last = newDifferences
+        }
+
+        // technically not needed, but conforming to the example
+        last.addLast(0L)
+
+        for (i in differences.size - 1 downTo 1) {
+            if (isPart2) {
+                // just sticking it at the end to avoid array resizing
+                differences[i - 1].addLast(differences[i - 1].first() - differences[i].last())
+            } else {
+                differences[i - 1].addLast(differences[i - 1].last() + differences[i].last())
+            }
+        }
+
+        answers.add(differences[0].last())
+    }
+
+    return answers.sum()
+}
